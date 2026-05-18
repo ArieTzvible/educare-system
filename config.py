d9e923d7d11830818b11df53b89a22d9f46ddfec
ArieTzvible <a0548463928@gmail.com>
2026-05-18 12:28:15 +0300

    update config

--- config.py
+++ config.py
@@ -8,10 +8,3 @@ class Config:
     SECRET_KEY = os.getenv('SECRET_KEY')
     UPLOAD_FOLDER = os.getenv('UPLOAD_FOLDER')
     BACKUP_PATH = os.getenv('BACKUP_PATH')
-
-
-print('config.py - print 1')
-print('1. Config.SECRET_KEY =',Config.SECRET_KEY)
-print('2. Config.UPLOAD_FOLDER = ',Config.UPLOAD_FOLDER)
-print('3. Config.BACKUP_PATH =',Config.BACKUP_PATH)
-print('-------------')
